9c4a67e7db0b46e96cd4d7857105e3358b0bc81c
lyctea <396136337@qq.com>
2022-12-27 15:22:57 +0800

    feat: 调整代码结构

---
+++ src/components/navication/MobileHeader.client.tsx
@@ -0,0 +1,88 @@
+import {Link, useUrl, useCart, Image} from '@shopify/hydrogen';
+import type {EnhancedMenu} from '~/lib/utils';
+import {useWindowScroll} from 'react-use';
+
+import {
+  Heading,
+  IconAccount,
+  IconArrow,
+  IconBag,
+  IconMenu,
+  IconSearch,
+  Input,
+} from '~/components';
+
+function MobileHeader({
+  countryCode,
+  title,
+  isHome,
+  openCart,
+  openMenu,
+}: {
+  countryCode?: string | null;
+  title: string;
+  isHome: boolean;
+  openCart: () => void;
+  openMenu: () => void;
+}) {
+  const {y} = useWindowScroll();
+
+  const styles = {
+    button: 'relative flex items-center justify-center w-8 h-8',
+    container: `${
+      isHome
+        ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
+        : 'bg-contrast/80 text-primary'
+    } ${
+      y > 50 && !isHome ? 'shadow-lightHeader ' : ''
+    }flex lg:hidden items-center h-nav sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8`,
+  };
+
+  return (
+    <header role="banner" className={styles.container}>
+      <div className="flex items-center justify-start w-full gap-4">
+        <button onClick={openMenu} className={styles.button}>
+          <IconMenu />
+        </button>
+        <form
+          action={`/${countryCode ? countryCode + '/' : ''}search`}
+          className="items-center gap-2 sm:flex"
+        >
+          <button type="submit" className={styles.button}>
+            <IconSearch />
+          </button>
+          <Input
+            className={
+              isHome
+                ? 'focus:border-contrast/20 dark:focus:border-primary/20'
+                : 'focus:border-primary/20'
+            }
+            type="search"
+            variant="minisearch"
+            placeholder="Search"
+            name="q"
+          />
+        </form>
+      </div>
+
+      <Link
+        className="flex items-center self-stretch leading-[3rem] md:leading-[4rem] justify-center flex-grow w-full h-full"
+        to="/"
+      >
+        <Heading className="font-bold text-center" as={isHome ? 'h1' : 'h2'}>
+          {title}
+        </Heading>
+      </Link>
+
+      <div className="flex items-center justify-end w-full gap-4">
+        <Link to={'/account'} className={styles.button}>
+          <IconAccount />
+        </Link>
+        <button onClick={openCart} className={styles.button}>
+          <IconBag />
+          <CartBadge dark={isHome} />
+        </button>
+      </div>
+    </header>
+  );
+}
